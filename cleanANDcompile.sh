866682a3f94f6714dab06379a508799da9846128
Michele Nalli <michelenalli96@gmail.com>
2017-02-17 20:10:23 +0100

    Compilation edit

--- cleanANDcompile.sh
+++ cleanANDcompile.sh
@@ -1,6 +1,11 @@
 rm p1test.elf.*
 rm mikabooq.o
-arm-none-eabi-gcc -mcpu=arm7tdmi -c mikabooq.c -o mikabooq.o -I /usr/include/uarm
-arm-none-eabi-gcc -mcpu=arm7tdmi -c p1test.c -o p1test.o -I /usr/include/uarm
-arm-none-eabi-ld -T /usr/include/uarm/ldscripts/elf32ltsarm.h.uarmcore.x -o p1test.elf /usr/include/uarm/crtso.o /usr/include/uarm/libuarm.o p1test.o
+arm-none-eabi-gcc -mcpu=arm7tdmi -c mikabooq.c -o mikabooq.o -I .
+arm-none-eabi-gcc -mcpu=arm7tdmi -c p1test.c -o p1test.o -I . -I /usr/include/uarm
+
+arm-none-eabi-ld \
+   -T /usr/include/uarm/ldscripts/elf32ltsarm.h.uarmcore.x \
+   -o p1test.elf /usr/include/uarm/crtso.o \
+   /usr/include/uarm/libuarm.o p1test.o mikabooq.o
+
 elf2uarm -k p1test.elf
