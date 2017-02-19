1d721b2da1b84b7bd8eca2952b919b3bd152f7ff
Michele Nalli <michelenalli96@gmail.com>
2017-02-19 23:03:38 +0100

    Add files via upload

---
+++ p1test_compile.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+arm-none-eabi-gcc -mcpu=arm7tdmi -c -o mikabooq.o mikabooq.c -I . -include 'stdint.h'
+arm-none-eabi-gcc -mcpu=arm7tdmi -c -o p1test.o p1test.c -I . -I /usr/include/uarm
+
+arm-none-eabi-ld \
+   -T /usr/include/uarm/ldscripts/elf32ltsarm.h.uarmcore.x \
+   -o p1test.elf /usr/include/uarm/crtso.o \
+   /usr/include/uarm/libuarm.o p1test.o mikabooq.o
+
+elf2uarm -k p1test.elf
