da32881683037c1c3f68bd7c6fe21a1113a3388d
mnalli <michelenalli96@gmail.it>
2017-04-06 03:14:29 +0200

    pass-up prototype for fiq: done

--- p2test.c
+++ p2test.c
@@ -1,5 +1,8 @@
 
 void test(){
-    tprint(" The test has started\n");
-    HALT();
+    tprint(" The test has started\n"
+           " Ready to wait\n"
+        );
+    breakpoint();
+    WAIT();
 }
--- p2test_compile.sh
+++ p2test_compile.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-arm-none-eabi-gcc -mcpu=arm7tdmi -c -o main.o main.c \
+arm-none-eabi-gcc -mcpu=arm7tdmi -c -o initial.o initial.c \
 	-I . -I /usr/include/uarm \
 
 arm-none-eabi-gcc -mcpu=arm7tdmi -c -o p2test.o p2test.c \
@@ -13,7 +13,7 @@ arm-none-eabi-gcc -mcpu=arm7tdmi -c -o mikabooq.o mikabooq.c \
 arm-none-eabi-ld \
    -T /usr/include/uarm/ldscripts/elf32ltsarm.h.uarmcore.x \
    -o p2test.elf /usr/include/uarm/crtso.o \
-   /usr/include/uarm/libuarm.o main.o mikabooq.o p2test.o
+   /usr/include/uarm/libuarm.o initial.o mikabooq.o p2test.o
 
 elf2uarm -k p2test.elf
 
