513be6861f03727ae8effd5ee863259b9536dfe4
mnalli <michelenalli96@gmail.it>
2017-04-06 04:02:45 +0200

    syscall doesn't work as I expected to...

--- p2test.c
+++ p2test.c
@@ -1,8 +1,26 @@
+#include <libuarm.h>
 
-void test(){
+extern void breakpoint();
+
+void test_wait(){
     tprint(" The test has started\n"
            " Ready to wait\n"
         );
     breakpoint();
     WAIT();
+}
+
+void test_syscall(){
+    tprint("test_syscall has started\n");
+    breakpoint();
+    tprint("First syscall...\n"
+           "All arguments are 1\n");
+    SYSCALL(1, 1, 1, 1);
+    tprint("Second syscall...\n"
+           "All arguments are 0\n");
+    SYSCALL(0, 0, 0, 0);
+    tprint("Third syscall...\n"
+           "First two args are 1 the others are 0\n");
+    SYSCALL(1, 1, 0, 0);
+    HALT();
 }
