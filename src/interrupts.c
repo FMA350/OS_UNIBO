6e4c1f6d5f624ccd4693e2c01f300ae88d38d2ee
matteopincherle <matteo.pincherle@gmail.com>
2017-05-05 12:30:04 +0200

    Update interrupts.c

--- src/interrupts.c
+++ src/interrupts.c
@@ -18,6 +18,7 @@ extern unsigned int *timeSliceLeft;
 extern struct list_head readyq;
 extern struct tcb_t *current_thread;
 
+static void io_request(unsigned int);
 static void interval_timer_h();
 
 
@@ -51,11 +52,55 @@ void states_init(){
 void interrupt_h() {
     // TODO: check pending interrupts in priority order
     interval_timer_h();
+    //p points to bottom of the interrupt bitmap for external devices
+    //il primo byte che ha un interr pendente fa partire la gestione
+    void * p = (void *)0x00006fe0;
+    unsigned int i = 0;
+    while (i < 5){
+        if (*((unsigned int *)p)>0) {
+            //mi serve sapere quale dei 7 device di ogni tipo ha richiesto l'interr? volendo comunque lo si fa facilmente
+            io_request(i);
+            break;
+        }
+        p++;
+        i++;
+    }
+
 }
 
 /* Interval timer handler without pseudoclock facilities */
 static void interval_timer_h() {
     timeSliceLeft = (unsigned int *) getTIMER();
+    // salvataggio stato del processore
+    current_thread->t_s = *((state_t *) INT_OLDAREA); //memcpy implicita
+    // Inserimento del processo in coda
+    thread_enqueue(current_thread, &readyq);
+    scheduler();
+}
+
+
+static void io_request(unsigned int deviceType){
+    //serve?
+    #if 0
+    switch (deviceType){
+        case 0:
+            //disk handler
+            break;
+        case 1:
+            //tapes handler
+            break;
+        case 2:
+            //Network handler
+            break;
+        case 3:
+            //printers handler
+            break;
+        case 4:
+            //terminal handler
+            break;
+
+    }
+    #endif
     // salvataggio stato del processore
     current_thread->t_s = *((state_t *) INT_OLDAREA); //memcpy implicita
     // Inserimento del processo in coda
