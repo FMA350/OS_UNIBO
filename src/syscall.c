f1a1944e1becffd48a874da5fd1f099c4cc41336
mnalli <michelenalli96@gmail.it>
2017-09-24 22:33:45 +0200

    filan

--- src/syscall.c
+++ src/syscall.c
@@ -58,7 +58,7 @@ int send(struct tcb_t *dest, struct tcb_t *sender, uintptr_t msg)
              * Processo corrente
              * Qualsiasi processo (in tal caso non ha messaggi)
              */
-                if (current_thread != dest->t_pcb->sys_mgr)
+                if (sender != dest->t_pcb->sys_mgr)
                 // se il current_thread non è il sys_mgr della destinazione
                     deliver_directly(dest, (unsigned int) sender, msg);
                 // se lo è non facciamo nulla per non sovrascrivere il valore di ritornodelle system call
